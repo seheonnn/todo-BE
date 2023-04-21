f4834a4eefaaa56446359b01a992fd78881a3977
seheonnn <ho78901@naver.com>
2023-04-21 14:36:20 +0900

    Feat: updateMyInfo

--- src/main/java/com/example/todo/controller/UserController.java
+++ src/main/java/com/example/todo/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.example.todo.controller;
 
 import com.example.todo.dto.UserDTO;
+import com.example.todo.entities.UserEntity;
 import com.example.todo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Log4j2
 @RestController
@@ -18,9 +21,14 @@ public class UserController {
 
     // 내 정보 조회
     @GetMapping("")
-    public ResponseEntity<Void> getUserInfo(@RequestBody UserDTO user) {
-        userService.getUserInfo(user.getUserIdx());
-        return ResponseEntity.ok(null);
+    public ResponseEntity<Optional<UserEntity>> getMyInfo(@RequestBody UserDTO user) {
+        return ResponseEntity.ok(userService.getMyInfo(user.getUserIdx()));
 
+    }
+
+    // 내 정보 수정
+    @PostMapping("")
+    public ResponseEntity<Optional<UserEntity>> updateMyInfo(@RequestBody UserDTO user) {
+        return ResponseEntity.ok(userService.updateMyInfo(user));
     }
 }
--- src/main/java/com/example/todo/service/UserService.java
+++ src/main/java/com/example/todo/service/UserService.java
@@ -1,10 +1,14 @@
 package com.example.todo.service;
 
+import com.example.todo.config.RoleType;
+import com.example.todo.dto.UserDTO;
 import com.example.todo.entities.UserEntity;
 import com.example.todo.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.Optional;
 
 @Service
@@ -18,7 +22,23 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public Optional<UserEntity> getUserInfo(Long userIdx) {
+    public Optional<UserEntity> getMyInfo(Long userIdx) {
         return userRepository.findById(userIdx);
     }
+
+    public Optional<UserEntity> updateMyInfo(UserDTO user) {
+        UserEntity userEntity = userRepository.findById(user.getUserIdx()).orElse(null);
+        if (userEntity == null) {
+            return Optional.empty();
+        }
+        if (user.getName() != null) {
+            userEntity.setName(user.getName());
+        }
+        if (user.getProfileImage() != null) {
+            userEntity.setProfileImage(user.getProfileImage());
+        }
+        return Optional.of(userRepository.saveAndFlush(userEntity));
+
+    }
+
 }
