1f8fd9ebaaa3fa9e38b68b44befd48cf702e1084
seheonnn <ho78901@naver.com>
2023-05-09 23:35:09 +0900

    Fix: Cors Error

---
+++ src/main/java/com/example/todo/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.todo.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(HttpMethod.GET.name());
+    }
+}
