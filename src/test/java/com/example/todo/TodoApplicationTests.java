e5220d7108cb202a78069c28969e830719d1bd99
sumin <dltnals5763@naver.com>
2023-05-30 22:24:20 +0900

    feat: annotation update

--- src/test/java/com/example/todo/TodoApplicationTests.java
+++ src/test/java/com/example/todo/TodoApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.todo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
-@SpringBootTest
 class TodoApplicationTests {
 
 	@Test
