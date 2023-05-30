964f963b11304c34493cd058e7c6b725fbefa73d
sumin <dltnals5763@naver.com>
2023-05-30 22:35:13 +0900

    feat: annotation update

--- src/test/java/com/example/todo/TodoApplicationTests.java
+++ src/test/java/com/example/todo/TodoApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.todo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 class TodoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
